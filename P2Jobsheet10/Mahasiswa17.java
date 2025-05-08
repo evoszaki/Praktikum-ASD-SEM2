713d0bf7d32fcfe2c73a48d3fcea7055f3ff748d
zaki setyawan <kiminsetyawan@gmail.com>
2025-05-08 09:19:26 +0700

    OYEA

---
+++ P2Jobsheet10/Mahasiswa17.java
@@ -0,0 +1,16 @@
+package P2Jobsheet10;
+
+public class Mahasiswa17 {
+    String nim, nama, prodi, kelas;
+
+    public Mahasiswa17(String nim, String nama, String prodi, String kelas) {
+        this.nim = nim;
+        this.nama = nama;
+        this.prodi = prodi;
+        this.kelas = kelas;
+    }
+
+    public void tampilkanData() {
+        System.out.println(nim + " - " + nama + " - " + prodi + " - " + kelas);
+    }
+}
