bf9774ec358be6f37f29710049b89c65dee00652
zaki setyawan <kiminsetyawan@gmail.com>
2025-06-05 10:02:25 +0700

    Percobaan

---
+++ Pertemuan14/Node17.java
@@ -0,0 +1,14 @@
+package Pertemuan14;
+
+public class Node17 {
+    Mahasiswa17 mahasiswa17;
+    Node17 left, right;
+
+    public Node17() {
+    }
+
+    public Node17(Mahasiswa17 mahasiswa17) {
+        this.mahasiswa17 = mahasiswa17;
+        left = right = null;
+    }
+}
